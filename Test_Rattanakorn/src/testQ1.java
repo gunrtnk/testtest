bc79d35a19412cdb55d9525261187d59b3456e27
Rattanakorn Chongruaiklang <67591898+gunrtnk@users.noreply.github.com>
2023-03-12 16:12:05 +0700

    delete

--- Test_Rattanakorn/src/testQ1.java
+++ Test_Rattanakorn/src/testQ1.java
@@ -2,27 +2,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class testQ1 {
-    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-
-    public void setup() {
-        System.setOut(new PrintStream(out));
-    }
-
-
-    public void shouldDrawChristmasTree() {
-        // given
-        Christmastree tree = new Christmastree(4);
-
-        // when
-        tree.draw(4);;
-
-        // then
-        Assert.assertEquals("          *\r\n" +
-                "         ***\r\n" +
-                "        *****\r\n" +
-                "       *******\r\n", out.toString());
-    }
 
     /**
      * @param args
